9f4dd8fa5cf9a73ec3b39f1d0f44b8897c938159
Johnny Zheng <zhengjo@us.ibm.com>
2015-07-08 14:28:20 -0400

    still trying to deploy

--- src/com/bluetag/rest/resources/HelloWorldREST.java
+++ src/com/bluetag/rest/resources/HelloWorldREST.java
@@ -5,6 +5,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+@javax.ws.rs.ApplicationPath("resources")
 @Path("/test")
 public class HelloWorldREST {
 	
