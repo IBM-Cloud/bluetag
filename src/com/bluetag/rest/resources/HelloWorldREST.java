23308b628ac5af4bb093a62c74682ef5d2d5211d
Johnny Zheng <zhengjo@us.ibm.com>
2015-07-08 14:17:24 -0400

    testing helloworld rest

---
+++ src/com/bluetag/rest/resources/HelloWorldREST.java
@@ -0,0 +1,16 @@
+package com.bluetag.rest.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/test")
+public class HelloWorldREST {
+	
+	@GET 
+	@Produces(MediaType.TEXT_PLAIN)
+	public String sayHello(){
+		return "HELLO MY NAME IS JOHNNY";
+	}
+}
