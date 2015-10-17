e00c0c816b231e85523ef22e0dbc81b2aeea7617
Tom Seelbach <seelbach@us.ibm.com>
2015-10-16 20:43:15 -0400

    changed PathParam to QueryParam

--- src/com/bluetag/api/search/resources/SearchResource.java
+++ src/com/bluetag/api/search/resources/SearchResource.java
@@ -2,7 +2,8 @@ package com.bluetag.api.search.resources;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
+//import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -12,10 +13,9 @@ import com.bluetag.api.search.service.SearchService;
 public class SearchResource {
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/{searchString}")
-	public String searchUsers(@PathParam("searchString") String searchString){
+	public String searchUsers(@QueryParam("q") String queryString){
 		SearchService searchService = new SearchService();
-		System.out.println("SearchResource.searchUsers - searchString: "+ searchString);
-		return searchService.searchUsers(searchString);
+		System.out.println("SearchResource.searchUsers - queryString: "+ queryString);
+		return searchService.searchUsers(queryString);
 	}
 }
