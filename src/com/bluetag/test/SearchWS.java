94cc4071d903a0cdebbdfc4aa24294f8d78ad730
Tom Seelbach <seelbach@us.ibm.com>
2015-10-23 10:37:28 -0400

    initial push of BluetagTest

---
+++ src/com/bluetag/test/SearchWS.java
@@ -0,0 +1,35 @@
+package com.bluetag.test;
+//http://stackoverflow.com/questions/26452903/javax-websocket-client-simple-example
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class SearchWS {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
