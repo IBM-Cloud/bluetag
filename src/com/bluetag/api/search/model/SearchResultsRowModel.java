a72b5c24c49c04ed425186140ca4685f20057cfd
arshadm13 <arshadmu@us.ibm.com>
2015-11-23 12:32:18 -0500

    duplicate search models - need to merge with existing

---
+++ src/com/bluetag/api/search/model/SearchResultsRowModel.java
@@ -0,0 +1,37 @@
+package com.bluetag.api.search.model;
+
+import java.util.ArrayList;
+
+public class SearchResultsRowModel {
+	
+	private String id;
+	private ArrayList<Integer> order;
+	
+	private SearchResultsFieldsModel fields;
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public ArrayList<Integer> getOrder() {
+		return order;
+	}
+	
+	public void setOrder(ArrayList<Integer> order) {
+		this.order = order;
+	}
+	
+	public SearchResultsFieldsModel getFields() {
+		return fields;
+	}
+	
+	public void setFields(SearchResultsFieldsModel fields) {
+		this.fields = fields;
+	}
+	
+
+}
