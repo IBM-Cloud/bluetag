6e5a9736021195895bc005b46d05c2c3abee8464
arshadm13 <arshadmu@us.ibm.com>
2015-11-24 12:03:50 -0500

    cors filter

---
+++ src/com/bluetag/api/search/resources/CORSHeader.java
@@ -0,0 +1,40 @@
+package com.bluetag.api.search.resources;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+public class CORSHeader implements Filter {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletResponse servletResponse = (HttpServletResponse) response;
+		
+		servletResponse.setHeader("Access-Control-Allow-Origin", "*");
+		servletResponse.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Origin");
+		
+		chain.doFilter(request, response);
+		
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
