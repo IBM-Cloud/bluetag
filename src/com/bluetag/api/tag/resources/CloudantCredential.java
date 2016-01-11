8e0c02bf201e3c7d5f1d39be48cbcc4e04091bde
System Administrator <root@arshadmus-MacBook-Pro.local>
2016-01-11 15:11:22 -0500

    Environment variables for cloudant credentials

---
+++ src/com/bluetag/api/tag/resources/CloudantCredential.java
@@ -0,0 +1,84 @@
+package com.bluetag.api.tag.resources;
+
+import java.io.IOException;
+
+import com.ibm.json.java.JSONArray;
+import com.ibm.json.java.JSONObject;
+
+import java.util.logging.Logger;
+
+
+
+public class CloudantCredential {
+	private JSONArray cloudant;
+	private JSONObject cloudantInstance;
+	private JSONObject cloudantCredentials;
+	
+	private String cloudantUsername;
+	private String cloudantPassword;
+	private String cloudantURI;
+	
+	private static final Logger LOGGER = Logger.getLogger(CloudantCredential.class.getName());
+	
+	public CloudantCredential () {
+		
+		//Get VCAP_SERVICES environment varialbe from Bluemix - cloudant service should be bound to this service for
+		//environment variable to be present
+		//
+		//Alternatively - provide a manifest.yml where environment variables are provided
+		String VCAP_SERVICES = System.getenv("VCAP_SERVICES");
+		
+	if (VCAP_SERVICES != null) {	
+		try {
+			System.out.println("vcap is: " + VCAP_SERVICES);
+			JSONObject vcap = (JSONObject) JSONObject.parse(VCAP_SERVICES);
+			JSONArray credentials = (JSONArray) vcap.get("cloudantNoSQLDB");
+			
+			 cloudantInstance = (JSONObject) credentials.get(0);
+			 cloudantCredentials = (JSONObject) cloudantInstance.get("credentials");
+			
+			//System.out.println(cloudantCredentials);
+			 LOGGER.info("username is from VCAP_SERVICES: " + cloudantCredentials.get("username").toString());
+			 LOGGER.info("password is from VCAP_SERVICES: " + (cloudantPassword == null ? "null" : "******"));
+			 
+			 
+			//System.out.println(cloudantCredentials.get("password"));
+			
+			cloudantUsername = cloudantCredentials.get("username").toString();
+			cloudantPassword = cloudantCredentials.get("password").toString();
+			cloudantURI = cloudantCredentials.get("url").toString();
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+	 } else {
+		 
+		 //VCAP services does not exist so look for dbUsername, dbPassword and dbURI environment variables
+		 //When running locally - these environment variables need to be defined by user. Values should be
+		 //from a Cloudant service running in Bluemix
+		 cloudantUsername = System.getenv("dbUsername");
+		 LOGGER.info("username is from environment variable dbUsername: " + cloudantUsername);
+		 cloudantPassword = System.getenv("dbPassword");
+		 LOGGER.info("password is from environment variable dbPassword: " + (cloudantPassword == null ? "null" : "*****"));
+		 cloudantURI = System.getenv("dbURI");
+		
+		 
+	 }
+		
+	}
+	
+	public String getCloudantUsername() {
+		return cloudantUsername;
+	}
+	
+	public String getCloudantPassword() {
+		return cloudantPassword;
+	}
+	
+	public String getCloudantURI() {
+		return cloudantURI;
+	}
+	
+
+
+}
--- src/com/bluetag/api/tag/service/TagService.java
+++ src/com/bluetag/api/tag/service/TagService.java
@@ -13,6 +13,7 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
+import com.bluetag.api.tag.resources.CloudantCredential;
 import com.bluetag.model.NewTagModel;
 import com.bluetag.model.TagModel;
 import com.google.gson.Gson;
@@ -21,15 +22,16 @@ public class TagService {
 	private String successJson = "{\"result\": \"success\"}";
 	private String failJson = "{\"result\": \"something has gone horribly wrong. Please try again\"}";
 	private String authHeaderKey = "Authorization";
-	private String toConvert = "9885315c-7077-4788-bb1d-cecd6a3530ff-bluemix:3a27472537c70e3bd9dbf474a06bd0660b4bd08783176d168c2d1f51e1b24943";
-	private String authHeaderValue = "Basic "
-			+ DatatypeConverter.printBase64Binary(toConvert.getBytes());
 	private String acceptHeaderKey = "Accept";
 	private String acceptHeaderValue = "application/json";
 	private String contentHeaderKey = "Content-Type";
 	private String contentHeaderValue = "application/json";
-	private String cloudantURI = "https://9885315c-7077-4788-bb1d-cecd6a3530ff-bluemix:3a27472537c70e3bd9dbf474a06bd0660b4bd08783176d168c2d1f51e1b24943@9885315c-7077-4788-bb1d-cecd6a3530ff-bluemix.cloudant.com";
 
+	CloudantCredential cc = new CloudantCredential();
+	//private String toConvert = cc.getCloudantUsername() + ":" + cc.getCloudantPassword();
+	private String authHeaderValue = "Basic " + DatatypeConverter.printBase64Binary((cc.getCloudantUsername() + ":" + cc.getCloudantPassword()).getBytes());
+	private String cloudantURI = cc.getCloudantURI();
+	
 	public String updateTagged(NewTagModel newtag) {
 
 		CloseableHttpClient httpclient = HttpClients.createDefault();
