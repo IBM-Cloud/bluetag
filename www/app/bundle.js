c721c9e0a951dedd4668692f81b5bf70e36f4f84
arshadm13 <arshadmu@us.ibm.com>
2016-01-21 17:33:47 -0500

    abstracted backend URLs - update env-config.json to desired backend service routes

--- www/app/bundle.js
+++ www/app/bundle.js
@@ -1,3 +1,14 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+module.exports={
+	"register": "http://bluetag-register.mybluemix.net/" ,
+	"game": "http://bluetag-engine.mybluemix.net/",
+	"location": "ws://bluetag-location.mybluemix.net/",
+	"search": "ws://bluetag-search.mybluemix.net/",
+	"tag": "http://bluetag-tag.mybluemix.net/"
+
+}
+
+},{}],2:[function(require,module,exports){
 /*
 Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
 This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
@@ -313,3 +324,5 @@ function deg2rad(deg) {
 }
 
 })(document);
+
+},{"../env-config.json":1}]},{},[2]);
